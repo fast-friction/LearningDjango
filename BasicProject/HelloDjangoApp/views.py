8badafcceaea0d1364afae61e3c2ce2f59c53b84
fast-friction <54060627+fast-friction@users.noreply.github.com>
2020-02-18 23:41:44 +0900

    手順2-4終了

--- BasicProject/HelloDjangoApp/views.py
+++ BasicProject/HelloDjangoApp/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.shortcuts import render
 
 def index(request):
     now = datetime.now()
 
-    html_content = "<html><head><title>Hello,Django</title></head><body>"
-    html_content += "<strong>Hello Django!</strong> on "+ now.strftime("%A, %d %B,%Y at %X")
-    html_content += "</body></html>"
-
-    return HttpResponse(html_content)
-
-# Create your views here.
+    return render(
+        request,
+        "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
+        {
+            'title' : "Hello Django",
+            'message' : "Hello Django!",
+            'content' : " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
