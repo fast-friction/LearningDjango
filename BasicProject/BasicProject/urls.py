510706ec4d3e598feed63295a83dc864b1281560
fast-friction <54060627+fast-friction@users.noreply.github.com>
2020-02-19 22:38:43 +0900

    手順３全て終了

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -13,5 +13,6 @@ import HelloDjangoApp.views
 urlpatterns = [
     url(r'^$', HelloDjangoApp.views.index, name='index'),
     url(r'^home$', HelloDjangoApp.views.index,name='home'),
+    url(r'^about$',HelloDjangoApp.views.about,name='about'),
 
 ]
